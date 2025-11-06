4840085f147bab43668a25d7e271d0782ce33280
Nicholas Lenz <Nicholas Lenz>
2025-11-06 09:50:28 -0500

    Configure PAGER environment variable

--- home.nix
+++ home.nix
@@ -10,6 +10,7 @@
     EDITOR = "emacsclient -c";
     # Add color to manpages
     MANPAGER = "sh -c 'sed -u -e \\\"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\\\" | bat -p -lman'";
+    PAGER = "bat";
     NIXPKGS_ALLOW_UNFREE = "1";
   };
 
